066ac6003d2703aef79c2756557bf08670ecfe9e
pula-ashok <ashokpula0307@gmail.com>
2026-02-25 23:41:00 +0530

    be jwt token changes

---
+++ projectmanagementsystem/src/main/java/com/pula/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.pula.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY  = "afjasldkfsldflksjdflkjslkdfjsfsd";
+    public static final String JWT_HEADER = "Authorization";
+}
